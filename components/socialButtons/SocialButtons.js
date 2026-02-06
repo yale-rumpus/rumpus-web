3f652858dd95250d1be6c044934340cf86c786e4
CrankyTitanO7 <nedajtheawesome@gmail.com>
2026-02-06 15:30:43 -0500

    social buttons v2 (final)

--- components/socialButtons/SocialButtons.js
+++ components/socialButtons/SocialButtons.js
@@ -5,20 +5,21 @@ export default function SocialButtons() {
         {
             id: 1,
             href: "https://www.instagram.com/yale.rumpus",
-            className: styles.insta,
+            brandClass: styles.insta,
             icon: "fa-brands fa-instagram",
         },
         {
             id: 2,
             href: "https://x.com/YaleRumpus",
-            className: styles.x,
+            brandClass: styles.x,
             icon: "fa-brands fa-x-twitter",
         },
         {
             id: 3,
             href: "mailto:yalerumpus@gmail.com",
-            className: styles.gmail,
-            icon: "fa-solid fa-envelope",
+            brandClass: styles.gmail,
+            // CHANGED: Uses the Google Brand icon instead of generic envelope
+            icon: "fa-brands fa-google",
         },
     ];
 
@@ -26,9 +27,17 @@ export default function SocialButtons() {
         <div className={styles.backdrop}>
             <ul className={styles.fixedSocials}>
                 {socialLinks.map((link, index) => (
-                    <li key={link.id} className={styles.li} style={{ animationDelay: `${index * 0.15}s` }}>
-                        <a href={link.href} target="_blank" rel="noopener noreferrer" className={link.className}>
-                            <i className={link.icon}></i>
+                    <li key={link.id} className={styles.li} style={{ animationDelay: `${index * 0.1}s` }}>
+                        <a href={link.href} target="_blank" rel="noopener noreferrer">
+                            {/* Layer 1: Grey Base Icon */}
+                            <div className={styles.iconContainer}>
+                                <i className={`${link.icon} ${styles.greyIcon}`}></i>
+                            </div>
+
+                            {/* Layer 2: Colored "Slideover" Icon */}
+                            <div className={`${styles.mask} ${link.brandClass}`}>
+                                <i className={`${link.icon} ${styles.coloredIcon}`}></i>
+                            </div>
                         </a>
                     </li>
                 ))}
