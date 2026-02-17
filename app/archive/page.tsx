88d0eda3c7e597c22d036d0b5d46779df31d97f4
CrankyTitanO7 <nedajtheawesome@gmail.com>
2026-02-16 23:29:26 -0500

    archive styling v.5

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main style={{ padding: "2rem", maxWidth: 800 }}>
+        <main style={{ padding: "2rem", maxWidth: 800 }} className="archive-main">
             <h1>PDF Archive</h1>
 
             {Object.entries(archive)
                 .sort(([a], [b]) => Number(b) - Number(a))
                 .map(([year, files]) => (
-                    <section key={year} style={{ marginTop: "2rem" }}>
-                        <h2>{year}</h2>
-                        <ul>
+                    <section key={year} style={{ marginTop: "2rem" }} className="archive-main ">
+                        <h2 className="archive-header">{year}</h2>
+                        <ul className="issue-card">
                             {files.map((file: any) => (
-                                <li key={file.id}>
+                                <li key={file.id} className="linkto">
                                     <Link href={`/pdf/${file.id}`}>{file.title}</Link>
                                 </li>
                             ))}
